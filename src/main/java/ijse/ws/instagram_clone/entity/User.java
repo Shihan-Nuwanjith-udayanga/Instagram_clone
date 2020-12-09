57f5ff0b0a1a38ddb08b1065fcda7e1136f05cca
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2020-12-09 23:25:48 +0530

    update user entity

--- src/main/java/ijse/ws/instagram_clone/entity/User.java
+++ src/main/java/ijse/ws/instagram_clone/entity/User.java
@@ -1,37 +1,56 @@
 package ijse.ws.instagram_clone.entity;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
-    private String user_id;
-    private String user_name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int u_id;
+    private String name;
+    private String username;
     private String password;
-    private String email;
+    private String pro_pic;
 
     public User() {
-
     }
 
-    public User(String user_id, String user_name, String password, String email) {
-        this.user_id = user_id;
-        this.user_name = user_name;
+    public User(int u_id, String name, String username, String password, String pro_pic) {
+        this.u_id = u_id;
+        this.name = name;
+        this.username = username;
         this.password = password;
-        this.email = email;
+        this.pro_pic = pro_pic;
     }
 
-    public String getUser_id() {
-        return user_id;
+    public int getU_id() {
+        return u_id;
     }
 
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
+    public void setU_id(int u_id) {
+        this.u_id = u_id;
     }
 
-    public String getUser_name() {
-        return user_name;
+    public String getName() {
+        return name;
     }
 
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -42,21 +61,22 @@ public class User {
         this.password = password;
     }
 
-    public String getEmail() {
-        return email;
+    public String getPro_pic() {
+        return pro_pic;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPro_pic(String pro_pic) {
+        this.pro_pic = pro_pic;
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "user_id='" + user_id + '\'' +
-                ", user_name='" + user_name + '\'' +
+                "u_id=" + u_id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
+                ", pro_pic='" + pro_pic + '\'' +
                 '}';
     }
 }
