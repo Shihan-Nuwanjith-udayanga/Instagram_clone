d44461ac69abf3c4b9b2165c075879cebe648899
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2021-01-22 23:41:57 +0530

    all

--- src/main/java/ijse/ws/instagram_clone/controller/UserController.java
+++ src/main/java/ijse/ws/instagram_clone/controller/UserController.java
@@ -1,59 +1,27 @@
 package ijse.ws.instagram_clone.controller;
 
-import ijse.ws.instagram_clone.dto.UserDTO;
-import ijse.ws.instagram_clone.service.AmazonS3ClientService;
-import ijse.ws.instagram_clone.service.UserService;
-import ijse.ws.instagram_clone.util.StandardResponse;
+import ijse.ws.instagram_clone.entity.InstaUser;
+import ijse.ws.instagram_clone.service.InstaUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/user")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/admin")
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private InstaUserService instaUserService;
 
-    @Autowired
-    private AmazonS3ClientService amazonS3ClientService;
-
-
-    @PostMapping(value = "/register", consumes = { "multipart/form-data" })
-    public ResponseEntity<StandardResponse> register(UserDTO userDTO, @RequestPart(value = "pro_pic") MultipartFile file) {
-        try {
-            System.out.println(userDTO);
-            System.out.println(file);
-            System.out.println("register call : ");
-            this.amazonS3ClientService.uploadFileToS3Bucket(userDTO.getPro_pic(), true);
-            StandardResponse responseResponse = userService.registerUser(userDTO);
-            return new ResponseEntity<>(responseResponse, HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean createAdmin(@RequestBody() InstaUser user) {
+        return instaUserService.saveUser(user);
     }
 
-    @GetMapping("/login")
-    public ResponseEntity<StandardResponse> login(@RequestBody UserDTO userDto) {
-        try {
-            boolean islogged = userService.loginUser(userDto);
-            if (islogged) {
-                System.out.println("Ïs Loggin");
-                return new ResponseEntity<>(new StandardResponse(200, "true", null), HttpStatus.OK);
-            } else {
-                System.out.println("Loggin fail");
-                return new ResponseEntity<>(new StandardResponse(500, "Loggin fail", null), HttpStatus.OK);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public InstaUser getAdmin() {
+        return new InstaUser();
     }
+
 }
