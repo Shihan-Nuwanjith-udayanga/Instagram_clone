4da2e542b28cc6fc3617258e3e73a812c0696ad6
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2021-01-30 00:47:16 +0530

    all

---
+++ src/main/java/ijse/ws/instagram_clone/jwt/JwtAuthentication.java
@@ -0,0 +1,26 @@
+//package ijse.ws.instagram_clone.jwt;
+//
+//public class JwtAuthentication extends AbstractAuthenticationToken {
+//
+//    private final Object principal;
+//    private JWTClaimsSet jwtClaimsSet;
+//
+//    public JwtAuthentication(Object principal, JWTClaimsSet jwtClaimsSet, Collection<? extends GrantedAuthority> authorities) {
+//        super(authorities);
+//        this.principal = principal;
+//        this.jwtClaimsSet = jwtClaimsSet;
+//        super.setAuthenticated(true);
+//    }
+//
+//    public Object getCredentials() {
+//        return null;
+//    }
+//
+//    public Object getPrincipal() {
+//        return this.principal;
+//    }
+//
+//    public JWTClaimsSet getJwtClaimsSet() {
+//        return this.jwtClaimsSet;
+//    }
+//}
